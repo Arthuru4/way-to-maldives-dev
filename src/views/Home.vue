50e26b85ebff12a0221b92dfc977ae123fba627d
chernobay_a <chernobay_a@river-soft.net>
2019-08-18 00:44:31 +0300

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="WTM LOGO" src="https://15vjkf4cezxo5wuws1wy6yqr-wpengine.netdna-ssl.com/wp-content/uploads/Heart-Shaped.jpg" />
+    <Base msg="Welcome to WAY TO MALDIVES START PAGE" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Base from "@/components/Base.vue";
+
+export default {
+  name: "home",
+  components: {
+    Base
+  }
+};
+</script>
+
+<style scoped>
+img {
+  height: 20rem;
+  width: auto;
+}
+</style>
